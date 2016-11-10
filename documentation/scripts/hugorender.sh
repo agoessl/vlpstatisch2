f7dbd3798c89f6f21d8345696df2be75cb9f2e53
agoessl <angeliquegoessl@web.de>
2016-11-10 12:22:03 +0100

    vlpstatisch2 in vlpstatisch

--- documentation/scripts/hugorender.sh
+++ documentation/scripts/hugorender.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PATH=$PATH:~/Documents/git/vlp/vlpstatisch2/bookshelf:~/Documents/git/vlp/vlpstatisch2/bookshelf/themes:/usr/local/bin
+PATH=$PATH:~/Documents/git/vlp/vlpstatisch/bookshelf:~/Documents/git/vlp/vlpstatisch/bookshelf/themes:/usr/local/bin
 
 #hugo server --theme=hugo_theme_robust --buildDrafts --config="~/Documents/git/vlp/vlpstatisch2/bookshelf/config.toml" 
 
-hugo --theme=hugo_theme_robust --config="~/Documents/git/vlp/vlpstatisch2/bookshelf/config2.toml"
+hugo --theme=hugo_theme_robust --config="~/Documents/git/vlp/vlpstatisch/bookshelf/config2.toml"
